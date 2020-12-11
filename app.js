993e5087ad348ec90a788d9070bc1ee0359ba915
Azzouz <menaaziz27@gmail.com>
2020-12-11 16:41:36 +0200

    serving assests

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/index.html'));
+})
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/about.html'))
+})
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/contact.html'))
+})
+app.get('/post', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/post.html'))
+})
+
+app.listen(3000);
