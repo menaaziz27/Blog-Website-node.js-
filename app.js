5652d60b2479a567f915d37bac31696db1723fcd
Azzouz <menaaziz27@gmail.com>
2020-12-20 21:49:18 +0200

    search done

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
-const ejs = require('ejs')
+const bodyParser = require('body-parser');
+require('ejs')
+const BlogPost = require('./models/BlogPost');
 
 const path = require('path');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
+
 
 const app = express();
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+    const blogs = await BlogPost.find({});
+    console.log(blogs)
+    res.render('index', {
+        blogs
+    });
 })
 app.get('/about', (req, res) => {
     res.render('about')
@@ -19,6 +36,30 @@ app.get('/contact', (req, res) => {
 })
 app.get('/post', (req, res) => {
     res.render('post')
+})
+app.get('/posts/new', (req, res) => {
+    res.render('create')
+})
+
+// Create blog
+app.post('/posts/store', async (req, res) => {
+    // console.log(req.body);
+    // console.log(req.body.title);
+    // console.log(req.body.body);
+    await BlogPost.create(req.body)
+    res.redirect('/')
+})
+
+app.post('/posts/search', async (req, res) => {
+    // console.log(req.body.search)
+    const search = req.body.search;
+    const blogs = await BlogPost.find(); // return array of matched objects
+    const matchedBlogs = blogs.filter((obj) => obj.title.toLowerCase().includes(search.toLowerCase()) ||
+        obj.body.toLowerCase().includes(search.toLowerCase()))
+    console.log(matchedBlogs)
+    res.render('index', {
+        blogs: matchedBlogs
+    })
 })
 
 app.listen(3000);
