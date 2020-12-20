acf6505f75093a916ec2fb8b5a04a970f1667efd
Azzouz <menaaziz27@gmail.com>
2020-12-20 12:44:25 +0200

    layouts

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require('express');
+const ejs = require('ejs')
+
 const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views/index.html'));
+    res.render('index');
 })
 app.get('/about', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views/about.html'))
+    res.render('about')
 })
 app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views/contact.html'))
+    res.render('contact')
 })
 app.get('/post', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views/post.html'))
+    res.render('post')
 })
 
 app.listen(3000);
