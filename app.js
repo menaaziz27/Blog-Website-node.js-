b94fa5eabadc03a5d2d03c22f4f420b31738e1e5
Azzouz <menaaziz27@gmail.com>
2020-12-21 01:13:40 +0200

    middlewares use()

--- app.js
+++ app.js
@@ -18,10 +18,27 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(fileUpload());
+app.use(fileUpload()); // adds files property to the req object
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// custom middleware
+// all app.use() functions calls next()
+const customMiddleware = (req, res, next) => {
+    console.log('This is always called whenever visiting any route')
+    next();
+}
+app.use(customMiddleware);
+
+const validateMiddleware = (req, res, next) => {
+    if (req.files === null || req.body.title === null || req.body.body === null) {
+        return res.redirect('/posts/new')
+    }
+    next();
+}
+
+app.use('/posts/store', validateMiddleware);
 
 app.get('/', async (req, res) => {
     const blogs = await BlogPost.find({});
@@ -39,7 +56,7 @@ app.get('/contact', (req, res) => {
 
 // if we tried to type localhost:3000/post/123456
 app.get('/post/:id', async (req, res) => {
-    console.log(req.params.id); // 123456 printed out
+    // console.log(req.params.id); // 123456 printed out
     const blogPost = await BlogPost.findById(req.params.id);
     res.render('post', {
         blogPost
